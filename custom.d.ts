c65336ece8f968d4d42c34321745070a7f7ca860
endulum <71853916+endulum@users.noreply.github.com>
2024-03-14 20:53:34 -0700

    add Channel model

--- custom.d.ts
+++ custom.d.ts
@@ -1,9 +1,11 @@
 import { type IUserDocument } from './models/user'
+import { type IChannelDocument } from './models/channel'
 
 declare global {
   namespace Express {
     interface Request {
       requestedUser: IUserDocument // for urls where id is in the request params
+      requestedChannel: IChannelDocument // same as above but for channels
       loggingInUser: IUserDocument // for logging in
       authenticatedUser: IUserDocument // for protected routes
     }
---
+++ models/channel.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema, type Types } from 'mongoose'
+import type { IUserDocument } from './user'
+
+export interface IChannel {
+  title: string
+  admin: Types.ObjectId
+  users: Types.ObjectId[]
+}
+
+export interface IChannelDocument extends IChannel {
+  removeFromChannel: (user: IUserDocument) => Promise<void>
+  inviteToChannel: (user: IUserDocument) => Promise<void>
+  promoteToAdmin: (user: IUserDocument) => Promise<void>
+}
+
+const ChannelSchema = new Schema<IChannelDocument>({
+  title: { type: String, required: true }, // todo: add regex match property
+  admin: { type: Schema.ObjectId, ref: 'User', required: true },
+  users: [{ type: Schema.ObjectId, ref: 'User', required: true }]
+})
+
+// are these methods necessary? let's just try having them for now
+
+ChannelSchema.methods.removeFromChannel = async function (user: IUserDocument) {
+  this.users = this.users.filter((user: Types.ObjectId) => {
+    return user.toString() !== user.id.toString()
+  })
+  await this.save()
+}
+
+ChannelSchema.methods.inviteToChannel = async function (user: IUserDocument) {
+  this.users.push(user)
+  await this.save()
+}
+
+ChannelSchema.methods.promoteToAdmin = async function (user: IUserDocument) {
+  this.admin = user
+  await this.save()
+}
+
+const Channel = mongoose.model<IChannelDocument>('User', ChannelSchema)
+export default Channel
