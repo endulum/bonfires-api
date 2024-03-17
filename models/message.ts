f9e4736183f95f90724a6826d0fc5cd5f7ca5864
endulum <71853916+endulum@users.noreply.github.com>
2024-03-17 11:57:37 -0700

    add message model

---
+++ models/message.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, type Types, type Document } from 'mongoose'
+
+export interface IMessage {
+  channel: Types.ObjectId
+  user: Types.ObjectId
+  timestamp: Date
+  isRemoved: boolean
+  content: string
+}
+
+export interface IMessageDocument extends IMessage, Document {}
+
+const MessageSchema = new Schema<IMessageDocument>({
+  channel: { type: Schema.ObjectId, required: true },
+  user: { type: Schema.ObjectId, required: true },
+  timestamp: { type: Date, required: true, default: () => Date.now(), immutable: true },
+  isRemoved: { type: Boolean, default: true, required: true },
+  content: { type: String, required: true }
+})
+
+const Message = mongoose.model<IMessageDocument>('Message', MessageSchema)
+export default Message
