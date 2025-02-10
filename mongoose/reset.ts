f9dc5ad0b210ddd6081119b94b02bf321304928c
endulum <71853916+endulum@users.noreply.github.com>
2025-02-10 11:57:09 -0800

    adjust reset to include emptying supabase dev bucket

--- mongoose/reset.ts
+++ mongoose/reset.ts
@@ -1,8 +1,10 @@
 import "./connectionClient";
 import { wipeWithAdmin } from "./dev";
 import { token } from "../tests/helpers";
+import { empty } from "../supabase/client";
 
 async function main() {
+  await empty();
   await wipeWithAdmin();
   console.log(await token("admin"));
 }
