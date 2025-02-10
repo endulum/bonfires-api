f45d5e4b7463a5b1ff5096296849e04b6a30378d
endulum <71853916+endulum@users.noreply.github.com>
2025-02-09 19:22:46 -0800

    add docker db, add app.ts, add build scripts

--- app.ts
+++ app.ts
@@ -1,103 +1,60 @@
-// To successfully import a module: https://stackoverflow.com/questions/41292559/could-not-find-a-declaration-file-for-module-module-name-path-to-module-nam
+import dotenv from "dotenv";
+import express from "express";
+import asyncHandler from "express-async-handler";
+import cors from "cors";
+import logger from "morgan";
 
-import express, {
-  type Express,
-  type NextFunction,
-  type Request,
-  type Response
-} from 'express'
-import mongoose from 'mongoose'
-import morgan from 'morgan'
-import cors from 'cors'
-import 'dotenv/config'
-import { createServer } from 'http'
-import { Server } from 'socket.io'
-import compression from 'compression'
+import { errorHandler } from "./src/middleware/errorHandler";
+import { router } from "./src/router";
 
-import router from './routes'
-import helmet from 'helmet'
+dotenv.config({ path: ".env" });
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+console.warn(`environment: ${process.env.NODE_ENV}`);
 
-// todo: origin '*' is inherently unsafe. research and implement a safer method
+import "./mongoose/connectionClient";
 
-const app: Express = express()
-const server = createServer(app)
-const io = new Server(server, {
-  cors: { origin: '*' }
-})
-const port: string | undefined = process.env.PORT
-const uri: string | undefined = process.env.CONNECTION
-const secret: string | undefined = process.env.SECRET
+const app = express();
 
-if (secret === undefined) throw new Error('JWT secret is not defined.')
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 
-if (uri !== undefined) {
-  void mongoose.connect(uri)
-  const db = mongoose.connection
-  db.on('open', console.log.bind(console, 'mongo server connected'))
-  db.on('error', console.error.bind(console, 'mongo connection error'))
-} else throw new Error('Mongoose URI is not defined.')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(compression())
-app.use(helmet())
-app.use(cors({
-  origin: '*'
-}))
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+// suppress favicon request
+app.get("/favicon.ico", (_req, res) => res.status(204).end());
 
-// toggle this on and off to simulate latency when needed
-// app.use((req, res, next) => setTimeout(next, 750))
+// in dev, log method and given values
+if (process.env.ENV === "development") {
+  app.use(logger("dev"));
+  app.use(
+    asyncHandler(async (req, _res, next) => {
+      if (["POST", "PUT"].includes(req.method))
+        // eslint-disable-next-line no-console
+        console.dir(req.body, { depth: null });
+      if (req.method === "GET" && Object.keys(req.params).length > 1)
+        // eslint-disable-next-line no-console
+        console.dir(req.params, { depth: null });
+      next();
+    })
+  );
+  app.use(async (_req, _res, next) => {
+    setTimeout(next, 750);
+    // artificial delay to test visual loading in frontend
+  });
+}
 
-app.use(function (req, res, next) {
-  req.io = io
-  next()
-})
+app.use(router);
 
-app.use(router)
-
-app.use(function (req, res, next) {
-  res.sendStatus(404)
-})
-
-app.use((
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-): void => {
-  console.error(err.stack)
-  if ('statusCode' in err && typeof err.statusCode === 'number') {
-    res.status(err.statusCode).send(err.message)
-  } else {
-    res.status(500).send('Something went wrong.')
-  }
-})
-
-io.on('connection', async (socket) => {
-  socket.on('viewing channel', async (channelId: string) => {
-    await socket.join(channelId)
+app.use(
+  "*",
+  asyncHandler(async (_req, res) => {
+    res.sendStatus(404);
   })
+);
 
-  socket.on('leaving channel', async (channelId: string) => {
-    await socket.leave(channelId)
-  })
+app.use(errorHandler);
 
-  socket.on('you started typing', async (
-    channelId: string, userId: string, userName: string
-  ) => {
-    socket.to(channelId).emit('someone started typing', userId, userName)
-  })
-
-  socket.on('you stopped typing', async (
-    channelId: string, userId: string
-  ) => {
-    socket.to(channelId).emit('someone stopped typing', userId)
-  })
-})
-
-if (port !== undefined) {
-  server.listen(port, () => {
-    console.log(`⚡️ server starting at http://localhost:${port}`)
-  })
-} else throw new Error('Port is not defined.')
+const port = process.env.PORT ?? 3000;
+app.listen(port, () => {
+  console.warn(`⚡️ server starting at http://localhost:${port}`);
+});
