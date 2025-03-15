1f090a9fa664f3a743f78bfe318c42ad4afcd370
endulum <71853916+endulum@users.noreply.github.com>
2025-03-14 22:33:29 -0700

    clarify reset script usage of dev module for fake data

--- mongoose/reset.ts
+++ mongoose/reset.ts
@@ -7,14 +7,22 @@ async function main() {
   await emptyBucket();
   const admin = await dev.wipeWithAdmin();
 
-  await dev.inviteUsersToChannels(
-    await dev.createBulkUsers(20),
-    await dev.createBulkChannels(5, [admin]),
-    10
-  );
+  /*
+  
+  // create users
+  const users = await dev.createBulkUsers(20);
 
+  // create channels, with admin as owner
+  const channels = await dev.createBulkChannels(5, [admin])
+
+  // invite users en masse to channels (max 10 users per channel)
+  await dev.inviteUsersToChannels(users, channels, 10)
+
+  // populate existing channels with messages
   for (const channel of await Channel.find())
     await dev.createBulkMessages(channel, 100);
+
+  */
 }
 
 main()
